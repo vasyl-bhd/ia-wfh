f5d1a623ffed54687be2686af6ccd62ea4c2920b
vbohdanets <vbohdanets@hubspot.com>
2024-03-25 14:03:48 +0200

    Init chadcn

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
-import adapter from 'sveltekit-adapter-chrome-extension';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "sveltekit-adapter-chrome-extension";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess({})],
   adapter: adapter(),
-  appDir: "app"
-}
+  appDir: "app",
+};
